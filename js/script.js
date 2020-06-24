26800461b7ad03aa684907407cadce7c89575a2f
rahitashpaul <rahitash@hotmail.com>
2020-06-24 21:49:25 +0600

    owlcarousel

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        autoplay:true,
+        autoplaytimeout:3000,
+        autoplayHoverPuse:true,
+        loop:true,
+        margin:10,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+  });
